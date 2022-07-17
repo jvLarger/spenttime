eec81c0beae1b1a562bad303dc2250cd7fb915a4
João Victor <larger13@hotmail.com>
2022-07-17 17:14:16 -0300

    Project created

---
+++ backend/src/main/java/com/jlarger/spenttime/SpenttimeApplication.java
@@ -0,0 +1,13 @@
+package com.jlarger.spenttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpenttimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpenttimeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jlarger/spenttime/SpenttimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlarger.spenttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpenttimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
